2a60ad4924a0877cc3a2ba08adb572368a744491
NguyenThanhHug <nguyenthanhhung200497@gmail.com>
2021-07-13 18:11:39 +0700

    Using Fluent validation

--- eShopSolution.Data/Entities/Product.cs
+++ eShopSolution.Data/Entities/Product.cs
@@ -17,7 +17,7 @@ namespace eShopSolution.Data.Entities
         public bool? IsFeatured { get; set; }
         public List <Cart> Carts { get; set; }
         public List <OrderDetail> OrderDetails { get; set; }
-        public List <ProductTranslation> ProductTransactions { get; set; }
+        public List <ProductTranslation> ProductTranslations { get; set; }
         public List<ProductInCategory> ProductInCategories { get; set; }
         public List<ProductImage> ProductImages { get; set; }
     }
