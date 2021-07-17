01d1e5b1ac9c69eda5b9d8d4f4a76ba8bf7c4f7a
NguyenThanhHug <nguyenthanhhung200497@gmail.com>
2021-07-17 17:57:23 +0700

    create fluent validation

---
+++ eShopSolulution.AdminApp/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using eShopSolution.ViewModel.System.Users;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShopSolulution.AdminApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+            return View();
+        }
+    }
+}
